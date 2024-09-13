0903d055f034a63d8239eb15d303661255e1ca6b
Svetlana Perepelkina <perepelka.dev25@gmail.com>
2024-09-13 14:52:43 +0300

    Добавлен вывод результата вычисления при использовании BackgrowndWorker

--- L4/MainWindow.xaml.cs
+++ L4/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace L4
     public partial class MainWindow : Window
     {
         BackgroundWorker Bworker;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -31,66 +32,81 @@ namespace L4
             Bworker.WorkerReportsProgress = true;
             Bworker.ProgressChanged += worker_ProgressChanged;
             Bworker.RunWorkerCompleted += worker_RunWorkerCompleted;
-         
+            double step = Math.Round(N / 100);
+            start = Convert.ToInt32(window1.Start.Text);
+            stop = Convert.ToInt32(window1.Stop.Text);
+            N = Convert.ToInt32(window1.N.Text);
+            double a = start;
+            double b = stop;
         }
+
         int i = 0;
+        
         double start;
         double stop;
         double N;
+        double result = 0;
+        double h;
+        double x = 0;
         Window1 window1 = new Window1();
-
 
         //=========================================
         private void Dispatcher_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                CalculateAsynk();
-            }
-            catch { new Exception(); }
+            dispatcher.IsEnabled = false;
+            worker.IsEnabled = false;
+            CalculateAsynk();
         }
 
         private void worker_Click(object sender, RoutedEventArgs e)
         {
-
             pBar.Value = 0;
             window1.ShowDialog();
             Bworker.DoWork += worker_DoWork;
             Bworker.RunWorkerAsync(1000);
             dispatcher.IsEnabled = false;
             worker.IsEnabled = false;
+            double step = Math.Round(N / 100);
+            start = Convert.ToInt32(window1.Start.Text);
+            stop = Convert.ToInt32(window1.Stop.Text);
+            N = Convert.ToInt32(window1.N.Text);
+            Calc(start, stop, N);
+            res.Foreground = Brushes.LightGray;
+        }
+
+        private double Calc( double start, double stop, double N)
+        {
+            {
+                var step = Math.Round(N / 100);
+                h = (stop - start) / N;
+                double sum = 0;
+
+                for (var i = 1; i <= N; i++)
+                {
+                    x = start + i * h;
+                    sum += Math.Pow(x, 9);
+
+                    if (i % step == 0)
+                    {
+                        if (Bworker != null && Bworker.WorkerReportsProgress)
+                        {
+                            Bworker.ReportProgress((int)(i / step));
+                        }
+                    }
+
+                }
+                result = h * sum;
+               
+            }
+            return result;
         }
 
 
-        public void worker_DoWork(object sender, DoWorkEventArgs e)
+        public void  worker_DoWork(object sender, DoWorkEventArgs e)
         {
-            var step = Math.Round(N / 100);
-            double h;
-            double a = start;
-            double b = stop;
-            double x = 0;
-
-
-            h = (b - a) / N;
-            var sum = 0d;
-
-            for (var i = 1; i <= N; i++)
-            {
-                x = a + i * h;
-                sum += Math.Pow(x, 9);
-                var result = h * sum;
-                //Thread.Sleep(1);
-                if (i % step == 0)
-                {
-                    if (Bworker != null && Bworker.WorkerReportsProgress)
-                    {
-                        Bworker.ReportProgress((int)(i / step));
-                    }
-                }
-            }
-
             int max = (int)e.Argument;
             int result1 = 0;
+    
             
             for (int i = 0; i < max; i++)
             {
@@ -105,30 +121,27 @@ namespace L4
                Thread.Sleep(1);
 
             }
-            e.Result = result1;
+
+            Calc(start, stop, N);
+            e.Result = result;
         }
 
         void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             pBar.Value = e.ProgressPercentage;
-
         }
 
         void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            MessageBox.Show("Задача выполнена");
-            dispatcher.IsEnabled = true;
-            worker.IsEnabled = true;
+            //MessageBox.Show("Задача выполнена \n Результат:\t " + e.Result.ToString());
+            res.Foreground = Brushes.Green;
+            res.Text = e.Result.ToString();
+
         }
 
-       
         public Task CalculateAsynk()
         {
-            dispatcher.IsEnabled = false;
-            worker.IsEnabled = false;
-
             window1.ShowDialog();
-            var step = Math.Round(N / 100);
             start = Convert.ToInt32(window1.Start.Text);
             stop = Convert.ToInt32(window1.Stop.Text);
             N = Convert.ToInt32(window1.N.Text);
@@ -139,29 +152,27 @@ namespace L4
             double x = 0;
             
             h = (b - a) / N;
-            var sum = 0d;
+            double sum = 0;
             int max = 100;
-
+            res.Foreground = Brushes.LightGray;
             return Task.Run(() =>
-             {
-                 //MessageBox.Show(" " + h + "\n" + a + "\n" + b);
-                 int progressPercentage = Convert.ToInt32(((double)i / max) * 100);
+            {
                  for (var i = 1; i <= N; i++)
                  {
+
                      x = a + i * h;
                      sum += Math.Pow(x, 9); ;
-                     // MessageBox.Show(""  + x +"\n" + sum);
-                     // if (i % step == 0)
-                     {
-                         Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => pBar.Value += (i / step)));
-                         step++;
-                     }
 
+                    if (i % 42 == 0)
+                    {
+                         int progressPercentage = Convert.ToInt32(((double)i / max) * 1100);
+                         Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => pBar.Value += progressPercentage));
+                     }
                  }
                  var result = (h * sum);
-                 MessageBox.Show("Результат вычислений\t" + result);
+                 MessageBox.Show("Задача выполнена \n Результат:\t" + result);          
              });
-                
+           
         }
     }
 }
